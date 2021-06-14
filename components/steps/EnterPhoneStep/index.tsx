ee30e194d543375795c822ab61f45015b8a84a90
Nikolay Stadnik <nick.stdnk@gmail.com>
2021-06-15 01:08:44 +0300

    add steps\EnterPhoneStep

---
+++ components/steps/EnterPhoneStep/index.tsx
@@ -0,0 +1,52 @@
+import { useState, useContext } from 'react';
+import clsx from 'clsx';
+import NumberFormat from 'react-number-format';
+import { WhiteBlock } from '../../WhiteBlock';
+import { Button } from '../../Button';
+import { StepInfo } from '../../StepInfo';
+
+import styles from './EnterPhoneStep.module.scss';
+import { MainContext } from '../../../pages';
+
+type InputValueState = {
+  formattedValue: string;
+  value: string;
+};
+
+export const EnterPhoneStep = () => {
+  const { onNextStep } = useContext(MainContext);
+  const [values, setValues] = useState<InputValueState>({} as InputValueState);
+
+  const nextDisabled = !values.formattedValue || values.formattedValue.includes('_');
+
+  return (
+    <div className={styles.block}>
+      <StepInfo
+        icon="/static/phone.png"
+        title="Enter your phone #"
+        description="We will send you a confirmation code"
+      />
+      <WhiteBlock className={clsx('m-auto mt-30', styles.whiteBlock)}>
+        <div className={clsx('mb-30', styles.input)}>
+          <img src="/static/BY.png" alt="flag" width={24} />
+          <NumberFormat
+            className="field"
+            format="+### (##) ###-##-##"
+            mask="_"
+            placeholder="+375 (29) 805-81-01"
+            value={values.value}
+            onValueChange={({ formattedValue, value }) => setValues({ formattedValue, value })}
+          />
+        </div>
+        <Button disabled={nextDisabled} onClick={onNextStep}>
+          Next
+          <img className="d-ib ml-10" src="/static/arrow.svg" />
+        </Button>
+        <p className={clsx(styles.policyText, 'mt-30')}>
+          By entering your number, you’re agreeing to our Terms of Service and Privacy Policy.
+          Thanks!
+        </p>
+      </WhiteBlock>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState, createContext } from 'react'
 import { ChooseAvatarStep } from '../components/steps/ChooseAvatarStep';
 import { EnterNameStep } from '../components/steps/EnterNameStep';
+import { EnterPhoneStep } from '../components/steps/EnterPhoneStep';
 import { TwitterStep } from '../components/steps/TwitterStep';
 import { WelcomeStep } from "../components/steps/WelcomeStep"
 
@@ -8,7 +9,8 @@ const stepsComponents = {
   0: WelcomeStep,
   1: EnterNameStep,
   2: TwitterStep,
-  3: ChooseAvatarStep
+  3: ChooseAvatarStep,
+  4: EnterPhoneStep
 }
 
 type MainContextProps = {
